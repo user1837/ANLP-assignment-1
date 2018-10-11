ff43996a85c61257824680878c3835e3903d23c0
Tiana Gale <tiana_gale@byu.edu>
2018-10-11 18:42:13 +0100

    Added random string generation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from random import random
 from math import log
 from collections import defaultdict
+import numpy
 
 tri_counts=defaultdict(int) #counts of all trigrams in input
 bi_counts=defaultdict(int) #counts of all bigrams in input
@@ -14,6 +15,7 @@ def preprocess_line(line):
     line = re.sub(r'\n', '', line) # removes newline character at the end of each line
     line = re.sub(r'[^A-Za-z.\s\d]', '', line)
     line = line.lower()
+    line = '##' + line + '#'
     return line
 
 print(preprocess_line('¿Sería apropiado que usted, Señora Presidenta, escribiese una carta'))
@@ -26,6 +28,47 @@ def calculate_mle_prob(tri_counts, bi_counts):
         mle_probs[key] = tri_counts[key] / history_count
     return mle_probs
 
+def generate_from_LM(distribution):
+    random_sequence = "#"
+    for i in range(299):
+        bigram = random_sequence[-2:]
+        if bigram[-1] == '#':
+            random_sequence = random_sequence + "#"
+            bigram = random_sequence[-2:]
+        random_sequence = random_sequence + append_char(bigram, distribution)
+
+        #print(random_sequence)
+    return random_sequence
+
+def normalize_probs(probs):
+    normalized_probs = []
+    for prob in probs:
+        normalized_probs.append(prob / sum(probs))
+    return normalized_probs
+
+def append_char(bigram, distribution):
+    possible_chars = []
+    probs = []
+    for key in distribution.keys():
+        if key[0:2] == bigram:
+            possible_chars.append(key[-1])
+            probs.append(distribution[key])
+    #print(possible_chars)
+    normalized_probs = normalize_probs(probs)
+    random_list = numpy.random.choice(possible_chars, size=None, replace=True, p=normalized_probs)
+    #print(random_list)
+    random_char = random_list[0]
+    return random_char
+
+def get_br_en_distribution():
+    br_en_distribution = {}
+    with open('model-br.en') as f:
+        for line in f:
+            line = re.sub(r'\n', '', line)
+            trigram = line[0:3]
+            prob = float(line[4:])
+            br_en_distribution[trigram] = prob
+    return br_en_distribution
 
 #here we make sure the user provides a training filename when
 #calling this program, otherwise exit with a usage error.
@@ -41,25 +84,28 @@ infile = sys.argv[1] #get input argument: the training file
 #beginning and end of each line. Depending on how you write the rest of
 #your program, you may need to modify this code.
 with open(infile) as f:
+    tri_counts.clear()
+    bi_counts.clear()
     for line in f:
         line = preprocess_line(line)
+        #print(line)
         for j in range(len(line)-(2)):
-            if j == 0:
-                trigram = '#'+line[j:j+2]
-                bigram = '#' + line[j:j+1]
-                tri_counts[trigram] += 1
-                bi_counts[bigram] += 1
+            # if j == 0:
+            #     trigram = '#'+line[j:j+2]
+            #     bigram = '#' + line[j:j+1]
+            #     tri_counts[trigram] += 1
+            #     bi_counts[bigram] += 1
 
             trigram = line[j:j+3]
             bigram = line[j:j+2]
             tri_counts[trigram] += 1
             bi_counts[bigram] += 1
 
-            if j == len(line)-3:
-                trigram = line[j+1:j+3] + '#'
-                tri_counts[trigram] += 1
-                bigram = line[j+1:j+3]
-                bi_counts[bigram] += 1
+            # if j == len(line)-3:
+            #     trigram = line[j+1:j+3] + '#'
+            #     tri_counts[trigram] += 1
+            #     bigram = line[j+1:j+3]
+            #     bi_counts[bigram] += 1
 
 
 #Some example code that prints out the counts. For small input files
@@ -73,4 +119,10 @@ for tri_count in sorted(tri_counts.items(), key=lambda x:x[1], reverse = True):
     print(tri_count[0], ": ", str(tri_count[1]))
 
 
-print(calculate_mle_prob(tri_counts, bi_counts))
+# mle_probs = calculate_mle_prob(tri_counts, bi_counts)
+# print(generate_from_LM(mle_probs))
+# br_en_dist = get_br_en_distribution()
+# print(generate_from_LM(br_en_dist))
+br_en_distribution = get_br_en_distribution()
+print(generate_from_LM(br_en_distribution))
+print(generate_from_LM(calculate_mle_prob(tri_counts, bi_counts)))
